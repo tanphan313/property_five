2d37419e266f9949adc6cc2118f51105980bb547
tanphan313 <tanphan.bka@gmail.com>
2020-03-20 05:20:04 -0700

    crud products admin v1.1

--- app/presenters/product_form_presenter.rb
+++ app/presenters/product_form_presenter.rb
@@ -17,6 +17,8 @@ class ProductFormPresenter
     product_images.map do |img|
       {
         id: img.id,
+        description: img.description,
+        master: img.master,
         small: Utils.full_asset_url(img.small_url),
         medium: Utils.full_asset_url(img.medium_url)
       }
