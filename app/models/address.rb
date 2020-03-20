6ef6939aab6c8b30dedba2c2540b737de096dd1c
tanphan313 <tanphan.bka@gmail.com>
2020-03-19 22:55:13 -0700

    update code

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
 class Address < ApplicationRecord
+  belongs_to :city
+  belongs_to :district
+  belongs_to :ward, optional: true
+  belongs_to :product
+
+  with_options presence: true do
+    validates :city_id, :district_id, :full_name
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :product_type
   belongs_to :product_category
+
+  has_one :address
+
+  with_options presence: true do
+    validates :title, :product_type_id, :product_category_id, :description, :contact_mobile_phone
+    validates :contact_mobile_phone, numericality: {only_integer: true}
+  end
+  validates :contact_phone, numericality: {only_integer: true}, allow_blank: true
+  validates :facade, :entrance, numericality: {only_integer: true}, allow_blank: true
+
+  enum house_direction: %i(none north north_east east south_east south south_west west north_west)
+  enum bacony_direction: %i(none north north_east east south_east south south_west west north_west)
 end
