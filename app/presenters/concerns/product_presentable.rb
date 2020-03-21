89f54ea090741fc79a020a562b7b9c8d9dd0e272
tanphan313 <tanphan.bka@gmail.com>
2020-03-21 09:37:40 -0700

    update code

--- app/presenters/concerns/product_presentable.rb
+++ app/presenters/concerns/product_presentable.rb
@@ -4,7 +4,7 @@ module ProductPresentable
   delegate :id, :title, :price, :project, :acreage, :address, :editor_email, :editor_type, :price,
     :product_type_id, :product_category_id, :product_images, :description, :facade,
     :entrance, :house_direction, :balcony_direction, :num_floor, :num_bedroom, :num_toilet, :furniture,
-    :contact_name, :contact_address, :contact_phone, :contact_mobile_phone, :contact_email, to: :product
+    :contact_name, :contact_address, :contact_phone, :contact_mobile_phone, :contact_email, :created_at, :updated_at, to: :product
   delegate :master_image, :product_images, to: :product
 
   def master_image_small_url
