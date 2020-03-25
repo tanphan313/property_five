6c6e1700c85c10a8719034ac29547fd69adff090
tanphan313 <tanphan.bka@gmail.com>
2020-03-25 11:12:12 -0700

    update code

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ server "103.77.160.66", port: 22, roles: [:web, :app, :db], primary: true
 
 set :ssh_options, {
   user: fetch(:user),
-  keys: %w(~/.ssh/deploy_property_five.pem),
+  #keys: %w(~/.ssh/deploy_property_five.pem),
   forward_agent: true,
   auth_methods: %w(publickey)
 }
