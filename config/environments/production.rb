c207dc0d7de11a2792e2601c5554f1887cbced19
tanphan313 <tanphan.bka@gmail.com>
2020-04-07 05:45:21 -0700

    update code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_controller.asset_host = 'https://propertyfive.herokuapp.com'
+  config.action_controller.asset_host = 'https://nhathat.vn'
 end
